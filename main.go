bd158c93d737f19793c631ed7ef6242f6a423648
RockSteady <turtlecoin@gmx.com>
2020-03-15 05:02:42 -0500

    input handler, menu, read/write channels

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
+	"github.com/common-nighthawk/go-figure"
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/network"
 	peerstore "github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p/p2p/protocol/ping"
-
 	multiaddr "github.com/multiformats/go-multiaddr"
 	"github.com/sirupsen/logrus"
 )
@@ -23,18 +27,22 @@ func semverInfo() string {
 	var majorSemver, minorSemver, patchSemver, wholeString string
 	majorSemver = "0"
 	minorSemver = "0"
-	patchSemver = "0"
+	patchSemver = "1"
 
 	wholeString = majorSemver + "." + minorSemver + "." + patchSemver
 	return wholeString
 }
 
-func announce() {
-	logrus.Info(appName + " - v" + semverInfo())
+func ascii() {
+	myFigure := figure.NewFigure("karai", "shadow", true)
+	myFigure.Print()
 }
 
 func main() {
-	announce()
+
+	ascii()
+	fmt.Println("\nType \x1b[35m'menu'\x1b[0m to view a list of commands")
+	inputHandler()
 	ctx := context.Background()
 
 	node, err := libp2p.New(ctx,
@@ -86,4 +94,111 @@ func main() {
 	if err := node.Close(); err != nil {
 		panic(err)
 	}
+}
+func handleStream(s network.Stream) {
+	logrus.Debug("New stream")
+	rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+	go readData(rw)
+	go writeData(rw)
+}
+func readData(rw *bufio.ReadWriter) {
+	for {
+		str, _ := rw.ReadString('\n')
+
+		if str == "" {
+			return
+		}
+		if str != "\n" {
+			// Green console colour: 	\x1b[35m
+			// Reset console colour: 	\x1b[0m
+			fmt.Printf("\x1b[32m%s\x1b[0m> ", str)
+		}
+
+	}
+}
+
+func writeData(rw *bufio.ReadWriter) {
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		sendData, err := stdReader.ReadString('\n')
+
+		if err != nil {
+			panic(err)
+		}
+
+		rw.WriteString(fmt.Sprintf("%s\n", sendData))
+		rw.Flush()
+	}
+
+}
+func inputHandler() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+
+		if strings.Compare("help", text) == 0 {
+			menuHelp()
+		} else if strings.Compare("?", text) == 0 {
+			menuHelp()
+		} else if strings.Compare("menu", text) == 0 {
+			menuHelp()
+		} else if strings.Compare("version", text) == 0 {
+			logrus.Debug("Displaying version")
+			menuVersion()
+		} else if strings.Compare("peer-info", text) == 0 {
+			logrus.Debug("Displaying peer-info")
+			menuPeerInfo()
+		} else if strings.Compare("exit", text) == 0 {
+			menuExit()
+		} else if strings.Compare("quit", text) == 0 {
+			menuExit()
+		} else if strings.Compare("close", text) == 0 {
+			menuExit()
+		} else {
+			logrus.Error("\nwtf is " + text + "???")
+			logrus.Error("Please choose something I can actually do:")
+			menuHelp()
+		}
+
+	}
+}
+
+func menuHelp() {
+	fmt.Println("\n\x1b[35mversion \t\t \x1b[0mDisplays version")
+	fmt.Println("\x1b[35mcreate-wallet \t\t \x1b[0mCreate a TRTL wallet")
+	fmt.Println("\x1b[35mwallet-balance \t\t \x1b[0mDisplays wallet balance")
+	fmt.Println("\x1b[35mlist-servers \t\t \x1b[0mLists pinning servers")
+	fmt.Println("\x1b[35mpeer-info \t\t \x1b[0mDisplays IPFS peer address")
+	fmt.Println("\x1b[35mexit \t\t\t \x1b[0mQuit immediately")
+}
+func menuCreateWallet() {
+	fmt.Println("create a wallet")
+}
+func menuBalance() {
+	fmt.Println("display wallet balance")
+}
+func menuListPinServers() {
+	fmt.Println("list known pinning servers")
+}
+func menuPeerInfo() {
+
+	logrus.Info("\nThis is your IPFS node address:")
+	// fmt.Println(node.Addrs())
+}
+
+func menuVersion() {
+	fmt.Println(appName + " - v" + semverInfo())
+}
+
+func menuExit() {
+	// if err := node.Close(); err != nil {
+	// 	panic(err)
+	// }
+	fmt.Println("\nExiting!")
+	os.Exit(0)
 }
